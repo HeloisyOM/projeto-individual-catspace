dd4388284e3d9f8c0ce9321d3a20e988f5f40f78
HeloisyOM <heloisy.mota@sptech.school>
2026-09-05 16:04:00 -0300

    feat:add estrutura base html, front,js que eu tinha esquecido de subir

---
+++ app/componentes/backend/projeto-individual-catspace/projeto-individual-catspace/src/test/java/com/catspace/projeto_individual_catspace/ProjetoIndividualCatspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catspace.projeto_individual_catspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIndividualCatspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
